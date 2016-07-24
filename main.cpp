ec3e7ee158bec6037be976b711ff49aedebf8f54
nhershy <nhershy@gmail.com>
2016-07-24 13:12:10 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//Basic Binary Search
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int ary[] = {2,3,4,5,6,7,8,10,13,18,22,34,40,43,46,47,49,55,57,58,59,60};
+    int size = 22;
+    int pos = 0;
+    int target = 49;
+    bool found = false;
+    int first = 0;
+    int last = size - 1;
+    int middle;
+
+    while (!found && first <= last)
+    {
+        middle = (first+last)/2;
+        if (ary[middle] == target)
+        {
+            found = true;
+            pos = middle;
+        }
+        else if (ary[middle] > target)
+        {
+            last = middle -1;
+        }
+        else
+        {
+            first = middle+1;
+        }
+    }
+
+    cout << "found at pos " << pos;
+
+    return 0;
+}
